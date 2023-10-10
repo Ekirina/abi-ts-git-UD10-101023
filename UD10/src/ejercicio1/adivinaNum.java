86bbde2824208bafedc5bfeabf8b86876d436a09
Ainara <ainarabliz@gmail.com>
2023-10-10 23:03:46 +0200

    primeros toques

---
+++ UD10/src/ejercicio1/adivinaNum.java
@@ -0,0 +1,18 @@
+package ejercicio1;
+import javax.swing.JOptionPane;
+
+public class adivinaNum {
+
+	adivinaNum n1 = new adivinaNum();
+	
+	public static void main(String[] args) {
+
+		
+		do {
+		int n1=Integer.parseInt(JOptionPane.showInputDialog("Adivina el número entre 500. Pon alguno: "));
+		}while(n1 != aleatorio) {
+
+		}
+	
+	}
+}
