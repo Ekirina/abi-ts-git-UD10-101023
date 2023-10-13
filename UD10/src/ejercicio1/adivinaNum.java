ecdfc2ba01228a45c31ba11599a4740af1001de2
Ainara <ainarabliz@gmail.com>
2023-10-13 23:25:28 +0200

    Empezamos de nuevo

--- UD10/src/ejercicio1/adivinaNum.java
+++ UD10/src/ejercicio1/adivinaNum.java
@@ -3,6 +3,9 @@ import javax.swing.JOptionPane;
 
 public class adivinaNum {
 
+	int aleatorio = (int) (Math.floor(Math.random()*(500-1+1)+1));
+	int contador = 1;
+	
 	adivinaNum n1 = new adivinaNum();
 	
 	public static void main(String[] args) {
@@ -13,6 +16,18 @@ public class adivinaNum {
 		}while(n1 != aleatorio) {
 
 		}
+	try {	
+		if (n1 == aleatorio) {
+			System.out.println("¡Enhorabuena! Acertaste el número =) ");
+		}else if (n1 < aleatorio){
+			System.out.println("Tu número "+n1+ " es menor, prueba otra vez.");
+		}else if (n1 > aleatorio) {
+			System.out.println("Tu número "+n1+ " es mayor, prueba otra vez.");
+		}while(n1 != aleatorio) {
+			}
+	
+		}
+	}
 	
 	}
 }
