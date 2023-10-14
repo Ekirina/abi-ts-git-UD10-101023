e820133e129591e6970eab9946f24b5d349a9275
Ainara <ainarabliz@gmail.com>
2023-10-14 21:06:01 +0200

    Por fin funciona el programa después de salir el error

--- UD10/src/ejercicio1/adivinaNum.java
+++ UD10/src/ejercicio1/adivinaNum.java
@@ -1,27 +1,33 @@
 package ejercicio1;
+
 import javax.swing.JOptionPane;
 
 public class adivinaNum {
 
-	public void adivinaNum(){	
+	public void esadivinaNum(){	
 
 		int aleatorio = (int) (Math.floor(Math.random()*(500-1+1)+1));
 		int contador = 1;	
 		int num = 0;
-		
-		do {int n1=Integer.parseInt(JOptionPane.showInputDialog("Adivina el número entre 500. Pon alguno: "));
-			num = n1;
-			if (num != aleatorio && num < aleatorio) {
-				System.out.println("Tu número "+num+ " es menor, prueba otra vez.");
-				contador++;
-			}else if (num != aleatorio && num > aleatorio) {
-				System.out.println("Tu número "+num+ " es mayor, prueba otra vez.");
+
+		do {
+			try{
+				int n1=Integer.parseInt(JOptionPane.showInputDialog("Adivina el número entre 500. Pon alguno: "));
+				num = n1;
+				if (num != aleatorio && num < aleatorio) {
+					System.out.println("Tu número "+num+ " es menor, prueba otra vez.");
+					contador++;
+				}else if (num != aleatorio && num > aleatorio) {
+					System.out.println("Tu número "+num+ " es mayor, prueba otra vez.");
+					contador++;
+				}
+
+			}catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(null,"Debe de poner un número. El error es el siguiente:\n"+ e);
 				contador++;
 			}
-			
 		}while(num != aleatorio);
+		System.out.println("¡Enhorabuena! Acertaste el número y tus intentos son: "+contador);
 		
-			System.out.println("¡Enhorabuena! Acertaste el número y tus intentos son: "+contador);
-
-}
+	}	
 }
--- UD10/src/ejercicio1/mainApp.java
+++ UD10/src/ejercicio1/mainApp.java
@@ -4,7 +4,7 @@ public class mainApp {
 
 	public static void main(String[] args) {
 		adivinaNum sorteo = new adivinaNum ();
-		sorteo.adivinaNum();
+		sorteo.esadivinaNum();
 	}
 
 }
